78bd4eb01a4d43721ed1b1096004ee0c8c103a0f
GN030 <ykdfreeyx@163.com>
2022-09-29 20:12:05 +0800

    第一章

--- 01/f.c
+++ 01/f.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+void PrintN(int N);
+
 int main()
 {
     printf("Hello World!\n");
     printf("你好世界！\n");
-    int b;b=2;
+
+    PrintN(100);
+
     system("pause");
     return 0;
+}
+
+void PrintN(int N)
+{
+    int i;
+    for (i=1;i<=N;i++)
+    {
+        printf("%d\n",i);
+    }
+    return;
 }
