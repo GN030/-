8105a5fc6850355999a78bf0ef0e56fe6ccea92b
GN030 <ykdfreeyx@163.com>
2022-09-29 20:23:14 +0800

    1

--- 01/f.c
+++ 01/f.c
@@ -1,3 +1,6 @@
+// 实现函数PrintN，打印1-N全部正整数。
+// 当N过大时，递归实现函数会报错。
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,18 +11,26 @@ int main()
     printf("Hello World!\n");
     printf("你好世界！\n");
 
-    PrintN(100);
+    PrintN(100000);
 
     system("pause");
     return 0;
 }
 
+// void PrintN(int N)
+// {
+//     int i;
+//     for (i=1;i<=N;i++)
+//     {
+//         printf("%d\n",i);
+//     }
+//     return;
+// }
+
 void PrintN(int N)
 {
-    int i;
-    for (i=1;i<=N;i++)
-    {
-        printf("%d\n",i);
+    if(N){
+        PrintN(N-1);
+        printf("%d\n",N);
     }
-    return;
 }
